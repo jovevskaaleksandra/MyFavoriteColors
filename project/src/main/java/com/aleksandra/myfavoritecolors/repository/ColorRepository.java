e8033991bdeb1ad32f48d01f23b89ee469b2b553
Aleksandra Jovevska <74111425+jovevskaaleksandra@users.noreply.github.com>
2022-11-16 02:02:06 +0100

    commit

---
+++ project/src/main/java/com/aleksandra/myfavoritecolors/repository/ColorRepository.java
@@ -0,0 +1,13 @@
+package com.aleksandra.myfavoritecolors.repository;
+
+import com.aleksandra.myfavoritecolors.model.Color;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ColorRepository extends JpaRepository<Color, Long> {
+
+    Optional<Color> findColorByName(String name);
+}
---
+++ project/src/test/java/com/aleksandra/myfavoritecolors/MyFavoriteColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandra.myfavoritecolors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFavoriteColorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
