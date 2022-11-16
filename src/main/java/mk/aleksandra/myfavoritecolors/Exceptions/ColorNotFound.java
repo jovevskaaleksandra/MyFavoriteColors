89d05ae1ba1005297cc12113cb5106049bb80892
Aleksandra Jovevska <74111425+jovevskaaleksandra@users.noreply.github.com>
2022-11-16 02:27:47 +0100

    Delete src directory

--- src/main/java/mk/aleksandra/myfavoritecolors/Exceptions/ColorNotFound.java
+++
@@ -1,7 +0,0 @@
-package mk.aleksandra.myfavoritecolors.Exceptions;
-
-public class ColorNotFound extends Exception{
-    public ColorNotFound(String message){
-        super(String.format("%s",message));
-    }
-}
